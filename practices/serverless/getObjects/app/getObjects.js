23cf3ee7edd652df64e1c617632e80c36e6fe44b
dcreutz <dennis.creutz@prodyna.com>
2020-02-03 19:10:03 +0100

    Added get lambda.

---
+++ practices/serverless/getObjects/app/getObjects.js
@@ -0,0 +1,41 @@
+const { getAllObjects } = require('./services/sqlService');
+
+exports.handler = async (event, context) => {
+  try {
+    const DB_HOST = process.env['DB_HOST'];
+    const DB_PORT = process.env['DB_PORT'];
+    const DB = process.env['DB'];
+    const DB_USER = process.env['DB_USER'];
+    const DB_PW = process.env['DB_PW'];
+
+    const dbConfig = {
+      host: DB_HOST,
+      port: DB_PORT,
+      database: DB,
+      user: DB_USER,
+      password: DB_PW,
+    };
+
+    console.log('Get objects...');
+    const result = await getAllObjects(dbConfig);
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": '*',
+      },
+      body: JSON.stringify(result)
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": '*',
+      },
+      body: e.message
+    }
+  }
+};
